2271bb57357103410ca86e0c96bdef0002a4a3f2
costin.chelu <costin.chelu@endava.com>
2026-03-23 15:38:07 +0200

    08.

--- projects/07-routing-basics/src/app/greetings.component.ts
+++ projects/07-routing-basics/src/app/greetings.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-greetings',
-  template: `<p>Hello dear learner 👋🏾</p>`,
+  template: `<p>Hello dear learner!!!</p>`,
   standalone: true,
 })
 export class GreetingsComponent {}
