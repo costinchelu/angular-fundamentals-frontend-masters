2ee79e81e3222218b56af803418537645530edf2
costin.chelu <costin.chelu@endava.com>
2026-03-23 18:32:14 +0200

    11.

--- projects/11-dependency-injection/src/app/app.component.ts
+++ projects/11-dependency-injection/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject } from '@angular/core';
+import { UserService } from './user.service';
+import { User } from './data'
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ` <h1>User Listing</h1> `,
+  imports: [UserInfoComponent],
+  template: `
+    <h1>User Listing</h1>
+    @for (user of userData; track user.id) {
+      <app-user-info [user] = "(user)"/>
+    }
+  `,
 })
 export class AppComponent {
-  constructor() {}
+  userService: UserService = inject(UserService);
+
+  userData: User[] = [];
+
+  // async ngOnInit(): Promise<void> {
+  //   const data = await this.userService.getUserData();
+  //   this.userData = data;
+  // }
+
+  constructor() {
+    this.userService.getUserData().then(data => {
+      this.userData = data;
+    });
+  }
 }
