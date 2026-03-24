80f9471de66bbb28f0bd07b33baf02885c6c70a4
costin.chelu <costin.chelu@endava.com>
2026-03-24 16:48:18 +0200

    ..

--- projects/13-deferrable-views/src/app/posts/posts.component.ts
+++ projects/13-deferrable-views/src/app/posts/posts.component.ts
@@ -14,30 +14,10 @@ import { Post } from '../data';
     </article>
     }
   `,
-  styles: `
-    :host {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-around;
-    }
-    .title {
-      font-weight: bold;
-    }
-    .body {
-      font-style: italic;
-    }
-    .post {
-      padding: 20px;
-      border-radius: 5px;
-      margin: 10px 0;
-      background: white;
-      box-shadow: 1px 5px 4px 2px #00000017;
-      width: 400px;
-
-    }
-  `,
+  styleUrl: './posts.component.css',
 })
 export class PostsComponent implements OnInit {
+
   protected postData: Post[] = [];
 
   ngOnInit() {
